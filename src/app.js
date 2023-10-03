af14599402a8db9967684e08a56efc3c5d2ae724
kshenukaishan <kshenukaishan@outlook.com>
2023-10-03 12:03:09 +0530

    Second commit with modification of modularised files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,67 @@
-const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const mongoose = require("mongoose");
+const Customer = require("./models/customer");
 
-console.log(uuidv4());
+const app = express();
+mongoose.set("strictQuery", false);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const PORT = process.env.PORT || 3000;
+const connection = process.env.URI;
+
+const customers = [
+  { name: "Ishan", industry: "IT" },
+  { name: "Krishan", industry: "BK" },
+  { name: "Yakusha", industry: "KT" },
+];
+
+const customer = new Customer({
+  name: "John",
+  industry: "IT",
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.get("/customers", async (req, res) => {
+  try {
+    const result = await Customer.find();
+    res.json({ customers: result });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/customers", async (req, res) => {
+  console.log(req.body);
+  const customer = new Customer(req.body);
+  try {
+    await customer.save();
+    res.status(201).json({ customer });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+
+app.post("/", (req, res) => {
+  res.send("Post request");
+});
+
+const start = async () => {
+  try {
+    await mongoose.connect(connection);
+    app.listen(PORT, () => {
+      console.log("App listening in port: " + PORT);
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+start();
---
+++ src/models/customer.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  industry: String,
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
